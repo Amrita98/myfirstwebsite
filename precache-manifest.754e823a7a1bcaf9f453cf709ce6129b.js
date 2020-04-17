3e95b28e6eee772c1597a37a647cfab165a764e5
Amrita98 <bhattacharjeeamrita10@gmail.com>
2020-04-17 23:47:51 +0530

    Updates

---
+++ precache-manifest.754e823a7a1bcaf9f453cf709ce6129b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d96a6836aa461cfb4307f65ac3d6b3",
+    "url": "/myfirstwebsite/index.html"
+  },
+  {
+    "revision": "be35e678074ea2591d63",
+    "url": "/myfirstwebsite/static/css/main.4405334b.chunk.css"
+  },
+  {
+    "revision": "8c5730a3d839f2f76cac",
+    "url": "/myfirstwebsite/static/js/2.f23b5938.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myfirstwebsite/static/js/2.f23b5938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be35e678074ea2591d63",
+    "url": "/myfirstwebsite/static/js/main.bab6a4f2.chunk.js"
+  },
+  {
+    "revision": "486f512a7cf4e4e11f57",
+    "url": "/myfirstwebsite/static/js/runtime-main.1c122d21.js"
+  },
+  {
+    "revision": "c163a1cd7bbfcb4df4da334b6268faec",
+    "url": "/myfirstwebsite/static/media/bg.c163a1cd.jpg"
+  }
+]);
